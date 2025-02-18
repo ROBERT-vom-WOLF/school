744b1f0ba233ad076526c8a27cadeef0c49f9ef2
Robin Hildwein <dev.robert.mcp@gmail.com>
2025-02-18 08:22:38 +0100

    fixed asserts

--- BFK-7_Projektaufgabe/Teil2/src/code/python/main.py
+++ BFK-7_Projektaufgabe/Teil2/src/code/python/main.py
@@ -13,7 +13,6 @@ class App:
 
         assert "main.py" in listdir(getcwd()), "Programm can only be executed from the folder it lies in itself!"
         assert path.isfile(config_path), "No config-file found! Check out the README.md of this Project inside 'school/BFK-7_Projektaufgabe/README.md'"
-        assert path.isfile(database_path), "No database-file found!"
 
         # load my config from my json file
         with open(config_path, "r", encoding="utf-8") as file:
